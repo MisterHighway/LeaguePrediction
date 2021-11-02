1e0245f45240563781c7d9c7d9bf9b235e244866
16960 <philip.kneemeyer@nordakademie.de>
2021-11-02 15:19:45 +0100

    completed datamodel

--- src/Model/data_model.py
+++ src/Model/data_model.py
@@ -1,30 +1,52 @@
-class Match: # 1 call
-    summoner1: None
-    summoner2: None
-    summoner3: None
-    summoner4: None
-    summoner5: None
-    summoner6: None
-    summoner7: None
-    summoner8: None
-    summoner9: None
-    summoner10: None # 10 x summoner callen
-    blue_win: None
-    red_win: None # not blue_win
-    t1_highest_winrate: None
-    t2_highest_winrate: None
-    t1_avg_winrate: None
-    t2_avg_winrate: None
+class SummonerMatch:
+
+    def __init__(self, puuid, game_time, in_game_stats):
+        self.puuid = puuid  # identifier of the summoner playing the match
+        self.game_time = game_time  # the time until one team won
+        self.in_game_stats = in_game_stats  # the summoners in game stats, such as damage done, etc
+
+    def to_string(self):
+        return '{ "puuid": "' + self.puuid + '", "game_time": ' + str(self.game_time) \
+               + ', "in_game_stats": ' + \
+               str(self.in_game_stats) + "}"
+
 
 class Summoner:
-    # feature related attributes
-    matches: [] # match ids -->     50 matches + 1 calls
-    champion: None
-    winrate_overall: None # winrate last 100 games
-    damage_done_avg: None
 
-class MatchDto:
-    # feature related attributes
-    # from participants[participantDto] mit summoner puuid
-    total_damage: None
-    total_healing: None
+    def __init__(self, match_history, champion):
+        self.match_history = match_history  # array of last matches
+        self.champion = champion  # champion in active game
+
+    def to_string(self):
+        match_history = "["
+        for m in self.match_history:
+            match_history += m.to_string() + ","
+        match_history = match_history[0:len(match_history) - 1]
+        match_history += "]"
+        return '{ "match_history": ' + match_history + ', "champion": ' + str(self.champion) + "}"
+
+
+class Match:
+
+    def __init__(self, summoner1, summoner2, summoner3, summoner4,
+                 summoner5, summoner6, summoner7, summoner8, summoner9, summoner10, win):
+        self.summoner1 = summoner1  # game participants
+        self.summoner2 = summoner2
+        self.summoner3 = summoner3
+        self.summoner4 = summoner4
+        self.summoner5 = summoner5
+        self.summoner6 = summoner6
+        self.summoner7 = summoner7
+        self.summoner8 = summoner8
+        self.summoner9 = summoner9
+        self.summoner10 = summoner10
+        self.win = win  # summoner 1-5 game won?
+
+    def to_string(self):
+        res = '{ "Summoner1":' + self.summoner1.to_string() + ', "Summoner2":' + self.summoner2.to_string() \
+               + ', "Summoner3":' + self.summoner3.to_string() + ', "Summoner4":' + self.summoner4.to_string() \
+               + ', "Summoner5":' + self.summoner5.to_string() + ', "Summoner6":' + self.summoner6.to_string() \
+               + ', "Summoner7":' + self.summoner7.to_string() + ', "Summoner8":' + self.summoner8.to_string() \
+               + ', "Summoner9":' + self.summoner9.to_string() + ', "Summoner10":' + self.summoner10.to_string() \
+               + ', "wins":' + str(self.win) + "}"
+        return res.replace("'", '"').replace("True", "true").replace("False", "false")
