b0e9bf224dfdbe694d2067362638c85b4816ecea
Izana Bizuneh <izana_gedion.bizuneh@nordakademie.de>
2021-11-08 16:36:52 +0100

    Created various functions for processing match data, creating features and training classifier

--- src/Model/file_writer.py
+++ src/Model/file_writer.py
@@ -1,9 +1,9 @@
 def write(text):
-    f = open('../Data/log_2.txt', 'a')
+    f = open('../Data/log.txt', 'a')
     f.write(str(text))
     f.close()
 
 
 def read_log():
-    f = open('../Data/log_2.txt', 'r')
+    f = open('../Data/log.txt', 'r')
     return f.read()
